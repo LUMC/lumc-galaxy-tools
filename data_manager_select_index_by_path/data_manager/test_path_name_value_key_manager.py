5cbacf152e6db55bf78c3693fd3414e6222fff11
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-06-28 16:23:49 +0200

    start on tests

---
+++ data_manager_select_index_by_path/data_manager/test_path_name_value_key_manager.py
@@ -0,0 +1,14 @@
+import pytest
+import json
+from pathlib import Path
+
+from path_name_value_key_manager import DataTable,check_tab,prefix_plus_extension_exists
+
+indexes_yml = Path(__file__) / "indexes.yml"
+test_data = Path(__file__) / "../test-data"
+
+def test_DataTable():
+    print()
+
+
+
