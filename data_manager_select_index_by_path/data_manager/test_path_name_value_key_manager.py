5b5f9fef85912ca8777238a6e71aeab44b8ace71
rhpvorderman <r.h.p.vorderman@lumc.nl>
2018-06-29 13:53:09 +0200

    style dingen

--- data_manager_select_index_by_path/data_manager/test_path_name_value_key_manager.py
+++ data_manager_select_index_by_path/data_manager/test_path_name_value_key_manager.py
@@ -4,65 +4,73 @@ import pytest
 import json
 from pathlib import Path
 
-from path_name_value_key_manager import DataTable, check_tab, prefix_plus_extension_exists
+from path_name_value_key_manager import DataTable
 
 indexes_yml = Path(__file__).parent / "indexes.yml"
 test_data = Path(__file__).parent / "../test-data"
 
 
-def DataTableTest(index_path: Path,
-                  data_table_name: str):
+def data_table_test(index_path: Path,
+                    data_table_name: str):
     dt = DataTable(index_path=index_path,
                    data_table_name=data_table_name,
                    indexes_properties_file=indexes_yml)
     data_manager_dict = dt.data_manager_dict
-    assert (data_manager_dict.get(data_table_name) != None)
+    assert (data_manager_dict.get(data_table_name) is not None)
     assert (data_manager_dict.get(data_table_name).get("path") == str(index_path))
 
-def test_DataTable():
-     dt = DataTable(index_path=test_data / "bwa_mem_index/EboVir3.fa",
-                  data_table_name="bwa_mem_indexes",
+
+def test_data_table():
+    dt = DataTable(index_path=test_data / "bwa_mem_index/EboVir3.fa",
+                   data_table_name="bwa_mem_indexes",
                    indexes_properties_file=indexes_yml)
-     assert(dt.name == "EboVir3")
-     assert(dt.value == "EboVir3")
-     assert(dt.dbkey == "EboVir3")
-     dm_json = json.loads(dt.data_manager_json)
-     print(dm_json)
-     assert(dm_json['bwa_mem_indexes'] == {"name":"EboVir3", "value":"EboVir3","dbkey":"EboVir3","path": str((test_data / "bwa_mem_index/EboVir3.fa"))})
+    assert (dt.name == "EboVir3")
+    assert (dt.value == "EboVir3")
+    assert (dt.dbkey == "EboVir3")
+    dm_json = json.loads(dt.data_manager_json)
+    print(dm_json)
+    assert (dm_json['bwa_mem_indexes'] == {"name": "EboVir3", "value": "EboVir3", "dbkey": "EboVir3",
+                                           "path": str((test_data / "bwa_mem_index/EboVir3.fa"))})
+
+
 def test_bowtie2_index():
-    DataTableTest(
+    data_table_test(
         index_path=test_data / "bowtie2_index/EboVir3",
         data_table_name="bowtie2_indexes")
 
+
 @pytest.mark.xfail
 def test_bowtie2_index_fail():
-    DataTableTest(
+    data_table_test(
         index_path=test_data / "bowtie2_index/EboVir3.fa",
         data_table_name="bowtie2_indexes")
 
+
 def test_bwa_index():
-    DataTableTest(index_path=test_data / "bwa_mem_index/EboVir3.fa",
-                  data_table_name="bwa_mem_indexes")
+    data_table_test(index_path=test_data / "bwa_mem_index/EboVir3.fa",
+                    data_table_name="bwa_mem_indexes")
 
 
 def test_bowtie_index():
-    DataTableTest(test_data / "bowtie_index/EboVir3.fa",
-                  data_table_name="bowtie_indexes")
+    data_table_test(test_data / "bowtie_index/EboVir3.fa",
+                    data_table_name="bowtie_indexes")
 
 
 def test_bowtie_index_color():
-    DataTableTest(test_data / "bowtie_index/color/EboVir3.fa",
-                  data_table_name="bowtie_indexes_color")
+    data_table_test(test_data / "bowtie_index/color/EboVir3.fa",
+                    data_table_name="bowtie_indexes_color")
 
 
 def test_hisat2_index():
-    DataTableTest(test_data / "hisat2_index/EboVir3",
-                  data_table_name="hisat2_indexes")
+    data_table_test(test_data / "hisat2_index/EboVir3",
+                    data_table_name="hisat2_indexes")
+
 
 def test_picard_index():
-    DataTableTest(test_data / "picard_index/EboVir3.fa",
-                  data_table_name="picard_indexes")
+    data_table_test(test_data / "picard_index/EboVir3.fa",
+                    data_table_name="picard_indexes")
+
 
 def test_sam_index():
-    DataTableTest(test_data / "fasta_indexes/EboVir3.fa",
-                  data_table_name="fasta_indexes")
+    data_table_test(test_data / "fasta_indexes/EboVir3.fa",
+                    data_table_name="fasta_indexes")
