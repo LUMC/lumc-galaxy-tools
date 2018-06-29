06e55b262c838834b78f8d1a3ed2bf8b4b7f1ce7
rhpvorderman <r.h.p.vorderman@lumc.nl>
2018-06-29 12:01:47 +0200

    add test

--- data_manager_select_index_by_path/data_manager/test_path_name_value_key_manager.py
+++ data_manager_select_index_by_path/data_manager/test_path_name_value_key_manager.py
@@ -10,14 +10,25 @@ from path_name_value_key_manager import DataTable,check_tab,prefix_plus_extensio
 indexes_yml = Path(__file__).parent / "indexes.yml"
 test_data = Path(__file__).parent / "../test-data"
 
-def test_bowtie2_index():
-    dt = DataTable(
-        index_path=test_data / "bowtie2_index/EboVir3",
-        data_table_name= "bowtie2_indexes",
-        indexes_properties_file= indexes_yml
-        )
+def DataTableTest(index_path: Path,
+                  data_table_name: str,
+                  name = None,
+                  value= None,
+                  dbkey = None):
+
+    dt = DataTable(index_path=index_path,
+                   data_table_name=data_table_name,
+                   indexes_properties_file=indexes_yml)
     data_manager_dict = dt.data_manager_dict
-    assert(data_manager_dict.get("bowtie2_indexes") != None)
+    assert(data_manager_dict.get(data_table_name) != None)
+    assert(data_manager_dict.get(data_table_name).get("path")==str(index_path))
+
+def test_bowtie2_index():
+        DataTableTest(
+        index_path=test_data / "bowtie2_index/EboVir3",
+        data_table_name= "bowtie2_indexes")
+
+
 
 
 
